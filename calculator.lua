cd059fa800d8c0085d12e67fc90d75b1f70be91f
Kusikureal <kusikugoog@gmail.com>
2024-09-03 11:50:23 +0300

    Update

---
+++ calculator.lua
@@ -0,0 +1,13 @@
+local function calculate(a, b, sign) 
+ if sign == "+" then
+  return a + b
+ elseif sign == "-" then
+  return a - b
+ elseif sign == "/" then
+  return a / b
+ elseif sign == "*" then
+  return a * b
+ end
+end
+
+print(calculate(10, 20, *)) 
